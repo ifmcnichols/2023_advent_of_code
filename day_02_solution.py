64b1b14fed82ac003bc39d83c7d922c6f7ef014f
Ian <ifmcnichols@arizona.edu>
2023-12-07 14:25:15 -0700

    Part 1

--- day_02_solution.py
+++ day_02_solution.py
@@ -1,16 +1,50 @@
-def calc_surface_area(length, width, height):
-    length = int(length)
-    width = int(width)
-    height = int(height)
-    sa = (2*length*width) + (2*width*height) + (2*height*length)
-    min_side = min([length, width, height])
-    return sa + min_side
+max_reds = 12
+max_greens = 13
+max_blues = 14
 
 
-total_sqft = 0
+def game_possible(input_dict):
+    if input_dict["reds"] > max_reds:
+        return False
+    if input_dict["blues"] > max_blues:
+        return False
+    if input_dict["greens"] > max_greens:
+        return False
+    return True
 
-with open("day_02_input.txt") as presents_file:
-    for line in presents_file.readlines():
-        l, w, h = line.split("x")
-        total_sqft += calc_surface_area(l, w, h)
-print('total sqft:', total_sqft)
+
+def parse_line(input_line):
+    input_line = input_line.strip()
+    game_id = int(input_line.split(":")[0].split(" ")[1])
+    reds = 0
+    blues = 0
+    greens = 0
+    for game in input_line.split(";"):
+        if "red" in game:
+            red = int(game.split(" red")[0].split(" ")[-1])
+            if red > reds:
+                reds = red
+        if "blue" in game:
+            blue = int(game.split(" blue")[0].split(" ")[-1])
+            if blue > blues:
+                blues = blue
+        if "green" in game:
+            green = int(game.split(" green")[0].split(" ")[-1])
+            if green > greens:
+                greens = green
+    return {"id": game_id, "reds": reds, "greens": greens, "blues": blues}
+
+
+inputs_file = open("day_02_input.txt", "r")
+lines = inputs_file.readlines()
+inputs_file.close()
+
+id_sum = 0
+
+for line in lines:
+    line_data = parse_line(line)
+    possible = game_possible(line_data)
+    if possible:
+        id_sum += line_data["id"]
+
+print("sum:", id_sum)
